b536ae05b5aedd13732fa86f604a6ad1c65cc7e6
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-08-19 12:24:58 -0300

    home finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import './App.css'
-import Footer from './components/footer/Footer'
-import Section1 from './components/section1/Section1'
-import Section2 from './components/section2/Section2'
-import Section3 from './components/section3/Section3'
-import Section4 from './components/section4/Section4'
-import Section5 from './components/section5/Section5'
-import Section6 from './components/section6/Section6'
-import Section7 from './components/section7/Section7'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
-
-
   return (
     <>
-      <Section1/>
-      <Section2/>
-      <Section3/>
-      <Section4/>
-      <Section5/>
-      <Section6/>
-      <Section7/>
-      <Footer/>
+      <Router basename="/Movies-ReactJS">
+        <Routes>
+          <Route path="/" element = {<Home/>}/>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
